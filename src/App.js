861e8470d11621c69feb3f5216d505084472a9eb
jackop <lolits2626@gmail.com>
2024-03-22 18:54:29 +0530

    All Done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import DisplayNotes from "./components/DisplayNotes";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import NotesArea from "./components/NotesArea";
 
 function App() {
-  const [notes,setNotes] = useState([]);
-  async function getDATA(){
+  const [notes, setNotes] = useState([]);
+  async function getDATA() {
     const resultData = await fetch("https://notes-api-navy.vercel.app/notes");
     let data = await resultData.json();
-  console.log(data);
-  data = data.reverse();
-  setNotes(data)
+    console.log(data);
+    data = data.reverse();
+    setNotes(data);
   }
-  useEffect(()=>{
+  useEffect(() => {
     getDATA();
-  },[])
+  }, []);
   return (
     <>
       <Navbar />
@@ -23,14 +23,19 @@ function App() {
       <hr />
       <div className="container-fluid">
         <div className="row d-flex justify-content-center">
-          {
-            notes.map((note)=><DisplayNotes title={note.title} desc={note.description} key={note._id} id={note._id} getDATA={getDATA}/>)
-          }
+          {notes.map((note) => (
+            <DisplayNotes
+              title={note.title}
+              desc={note.description}
+              key={note._id}
+              id={note._id}
+              getDATA={getDATA}
+            />
+          ))}
           {/* <DisplayNotes title={notes.title} desc={notes.description}/> */}
-         
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
